1b30e96dcb2ebb5cc068e2098f885f5813000a65
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2023-09-14 21:55:42 +0930

    feat: show other user's statuses

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { getSession, signIn, signOut, useSession } from 'next-auth/react';
-import { useReducer, useState } from 'react';
+import { useState } from 'react';
 import { ImmerReducer, useImmer } from 'use-immer';
 import clientPromise, { usersPromise } from '../lib/mongodb';
 import { Excersize } from '../state/Exdersize';
@@ -10,8 +10,9 @@ import { authOptions } from './api/auth/[...nextauth]';
 
 type ConnectionStatus = {
 	isConnected: boolean;
-	posts: Post[];
-	userStatus?: string;
+	// posts: Post[];
+	userStatus: string | null;
+	userStatuses: string[];
 };
 
 export const getServerSideProps: GetServerSideProps<ConnectionStatus> = async (
@@ -23,9 +24,9 @@ export const getServerSideProps: GetServerSideProps<ConnectionStatus> = async (
 		const session = await getSession(ctx);
 
 		const user =
-			session &&
-			(await authOptions.adapter!.getUserByEmail!(session.user.email!));
-		const userData = user && (await users.findOne({ id: user.id }));
+			session?.user?.email &&
+			(await authOptions.adapter!.getUserByEmail!(session.user.email));
+		const userData = user ? await users.findOne({ id: user.id }) : null;
 
 		// `await clientPromise` will use the default database passed in the MONGODB_URI
 		// However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
@@ -36,29 +37,48 @@ export const getServerSideProps: GetServerSideProps<ConnectionStatus> = async (
 		// Then you can execute queries against your database like so:
 		// db.find({}) or any of the MongoDB Node Driver commands
 
-		const posts = (
-			await client.db('posts').collection('posts').find({}).toArray()
-		).map(
-			(data) => new Post(data._id.toHexString(), data.title, data.content)
+		// const posts = (
+		// 	await client.db('posts').collection('posts').find({}).toArray()
+		// ).map(
+		// 	(data) => new Post(data._id.toHexString(), data.title, data.content)
+		// );
+
+		const allUsers = await Promise.all(
+			(
+				await users.find({}).toArray()
+			).map(async (u) => ({
+				authUser: await authOptions.adapter!.getUser!(u.id),
+				user: u,
+			}))
 		);
 
+		const userStatuses = allUsers.map(
+			(u) => `${u.authUser?.name ?? '??'}: ${u.user.status}`
+		);
 		console.log({
 			user,
 			userData,
+			userStatuses,
 			foo: 'foo',
 		});
 
 		return {
 			props: {
 				isConnected: true,
-				posts,
-				userStatus: userData?.status,
+				// posts,
+				userStatuses,
+				userStatus: userData?.status ?? null,
 			},
 		};
 	} catch (e) {
 		console.error(e);
 		return {
-			props: { isConnected: false, posts: [], userStatus: `${e}` },
+			props: {
+				isConnected: false,
+				// posts: [],
+				userStatuses: [],
+				userStatus: `${e}`,
+			},
 		};
 	}
 };
@@ -99,7 +119,8 @@ const workoutReducer: ImmerReducer<Workout, WorkoutAction> = (
 
 export default function Home({
 	isConnected,
-	posts,
+	// posts,
+	userStatuses,
 	userStatus,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
 	const [workoutHistory, setWorkoutHistory] = useState<Excersize[]>([]);
@@ -107,9 +128,9 @@ export default function Home({
 		createDefaultWorkout()
 	);
 
-	const [a, dispatchA] = useReducer(workoutReducer, [], () => [
-		createDefaultWorkout(),
-	]);
+	// const [a, dispatchA] = useReducer(workoutReducer, [], () => [
+	// 	createDefaultWorkout(),
+	// ]);
 
 	const previousSimilarWorkouts =
 		currentWorkout && currentWorkout.excersize.length > 3
@@ -142,53 +163,58 @@ export default function Home({
 				)}
 				<button onClick={() => signOut()}>Sign out</button>
 				<form action="/api/addPost" method="post">
-					<input defaultValue={userStatus} name="status" />
+					<input defaultValue={userStatus ?? ''} name="status" />
 					<button type="submit">Add Status</button>
 				</form>
+				<ul>
+					{userStatuses.map((status, i) => (
+						<li key={i}>{status}</li>
+					))}
+				</ul>
 			</div>
 		);
 	}
 }
 
-class Post {
-	constructor(
-		public id: string | null,
-		public title: string,
-		public content: string
-	) {}
+// class Post {
+// 	constructor(
+// 		public id: string | null,
+// 		public title: string,
+// 		public content: string
+// 	) {}
 
-	//todo: graphql
-	async write() {
-		const response = await fetch('http://localhost:3000/api/addPost', {
-			method: 'POST',
-			body: JSON.stringify({
-				title: this.title,
-				content: this.content,
-			}),
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-		});
+// 	//todo: graphql
+// 	async write() {
+// 		const response = await fetch('http://localhost:3000/api/addPost', {
+// 			method: 'POST',
+// 			body: JSON.stringify({
+// 				title: this.title,
+// 				content: this.content,
+// 			}),
+// 			headers: {
+// 				Accept: 'application/json',
+// 				'Content-Type': 'application/json',
+// 			},
+// 		});
 
-		const data = (await response.json()) as { _id: string };
+// 		const data = (await response.json()) as { _id: string };
 
-		this.id = data._id;
-	}
+// 		this.id = data._id;
+// 	}
 
-	static async get() {
-		const response = await fetch('http://localhost:3000/api/getPosts', {
-			headers: {
-				Accept: 'application/json',
-			},
-		});
+// 	static async get() {
+// 		const response = await fetch('http://localhost:3000/api/getPosts', {
+// 			headers: {
+// 				Accept: 'application/json',
+// 			},
+// 		});
 
-		const data = (await response.json()) as {
-			id: string;
-			title: string;
-			content: string;
-		}[];
+// 		const data = (await response.json()) as {
+// 			id: string;
+// 			title: string;
+// 			content: string;
+// 		}[];
 
-		return data.map((data) => new Post(data.id, data.title, data.content));
-	}
-}
+// 		return data.map((data) => new Post(data.id, data.title, data.content));
+// 	}
+// }
